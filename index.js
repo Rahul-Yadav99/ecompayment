32c679e3bece29e1f0e02c57045aab2945964ae7
rahul <rahul352sj@gmail.com>
2024-12-30 15:09:17 +0530

    fetch all payments on razarpay

--- index.js
+++ index.js
@@ -11,21 +11,30 @@ const instance = new Razorpay({
 
 app.listen('https://ecompayment.vercel.app/order')
 app.use(cors())
-app.use(body.urlencoded({extended : false}))
+app.use(body.urlencoded({ extended: false }))
 app.use(body.json())
 
 app.post('/order', async (req, res) => {
     try {
         const newOrder = await instance.orders.create({
-            amount: (req.body.amount*100),
+            amount: (req.body.amount * 100),
             currency: "INR",
             receipt: "CO_RP" + Date.now(),
 
         })
         res.json({
-            amount : newOrder.amount,
-            orderId : newOrder.id
+            amount: newOrder.amount,
+            orderId: newOrder.id
         })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+app.get('/payments', async (req, res) => {
+    try {
+        const payments = await instance.payments.all()
+        res.json(payments)
     } catch (error) {
         res.status(500).json(error)
     }
