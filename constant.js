a9fb53c23ae338d1640e3f0ecd189d5efe40ebea
rahul <rahul352sj@gmail.com>
2025-03-08 21:56:43 +0530

    final push

--- constant.js
+++ constant.js
@@ -1,2 +1,2 @@
-export const API_KEY = rzp_test_W1As5WgUmla9nV
-export const API_SECRET = R0HPzLmlfXMfXQVHs0wU4nNY
+export const API_KEY = 'rzp_test_W1As5WgUmla9nV'
+export const API_SECRET = 'R0HPzLmlfXMfXQVHs0wU4nNY'
